1aa9c86861bcf5963f08ee80a017c95dc82c9b28
kimeunsoo0324 <nice_eunice@naver.com>
2023-07-09 17:12:37 +0900

    0709

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,8 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 })
 
+/* 로그인 */
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
